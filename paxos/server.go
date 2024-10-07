093ae4a44e8d9a6630e3270e75bf8cbe1477ad52
money <lucasphone75@gmail.com>
2024-10-07 00:43:12 -0600

    use correct connection

--- paxos/server.go
+++ paxos/server.go
@@ -245,7 +245,8 @@ func (node *Node) Accept(
 
 								commitBuffer := make([]byte, 5)
 								commitBuffer[0] = OpCommit
-								binary.LittleEndian.PutUint32(buffer[1:5], current)
+								binary.LittleEndian.PutUint32(commitBuffer[1:5], current)
+								fmt.Printf("Committing up to %d\n", current)
 								for i := 0; i < node.Total; i++ {
 									if i == node.Index {
 										continue
