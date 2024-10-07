e250e0e3f6c4add62086698256c961a9391d1029
money <lucasphone75@gmail.com>
2024-10-07 17:45:14 -0600

    more debugs

--- paxos/server.go
+++ paxos/server.go
@@ -333,6 +333,7 @@ func (node *Node) Accept(
 							keyString := string(entry.key)
 							channel := node.RequestWaiter[keyString]
 							if channel != nil {
+								fmt.Printf("We closed the channel?\n")
 								close(channel)
 							}
 							delete(node.RequestWaiter, keyString)
