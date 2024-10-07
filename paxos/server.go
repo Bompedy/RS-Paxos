d4b66838e1005a1d7e97c2a3b5517b0d794108ca
money <lucasphone75@gmail.com>
2024-10-07 03:01:03 -0600

    use correct connection

--- paxos/server.go
+++ paxos/server.go
@@ -247,7 +247,7 @@ func (node *Node) Accept(
 
 								if atomic.LoadUint32(&nextEntry.acked) >= nextEntry.majority {
 									fmt.Printf("Had enough entries %d\n", i)
-									etcdWrite(nextEntry.key, nextEntry.value)
+									//etcdWrite(nextEntry.key, nextEntry.value)
 									fmt.Printf("Wrote it to etcd %d\n", i)
 									if nextEntry.condition != nil {
 										fmt.Printf("Closing entry condition in ack\n")
