4ed50c7386a6296616c1e28b042ff300e67919f2
money <lucasphone75@gmail.com>
2024-10-06 20:16:23 -0600

    test pipelining

--- paxos/server.go
+++ paxos/server.go
@@ -12,6 +12,10 @@ import (
 
 var OpWrite = uint8(0)
 var OpCommit = uint8(1)
+var OpForward = uint8(2)
+var OpAck = uint8(3)
+
+var CommitIndex uint32
 
 type Node struct {
 	Clients  []Client
@@ -21,6 +25,8 @@ type Node struct {
 	Quorum   int
 	Parity   int
 	Segments int
+	Index    int
+	Leader   int
 }
 
 type Log struct {
@@ -39,7 +45,6 @@ type Entry struct {
 func (node *Node) Connect(
 	local string,
 	nodes []string,
-	block func(key []byte, value []byte),
 ) error {
 	var waiter sync.WaitGroup
 	for _, address := range nodes {
@@ -64,33 +69,6 @@ func (node *Node) Connect(
 				mutex:      &sync.Mutex{},
 			}
 			node.Clients = append(node.Clients, client)
-			buffer := make([]byte, 4)
-
-			// this is the leader
-			go func() {
-				for {
-					err = client.Read(buffer)
-					if err != nil {
-						panic(err)
-					}
-					commitIndex := binary.LittleEndian.Uint32(buffer)
-					node.Log.Lock.Lock()
-					entry, exists := node.Log.Entries[commitIndex]
-					node.Log.Lock.Unlock()
-					//println("Does it exist: ", exists)
-
-					//fmt.Printf("Leader got response: %d, %d, %d, %v\n", commitIndex, acked, entry.majority, exists)
-					if exists && atomic.AddUint32(&entry.acked, 1) == entry.majority {
-						go func() {
-							block(entry.key, entry.value)
-							close(entry.condition)
-							node.Log.Lock.Lock()
-							delete(node.Log.Entries, commitIndex)
-							node.Log.Lock.Unlock()
-						}()
-					}
-				}
-			}()
 		}()
 	}
 
@@ -104,24 +82,10 @@ type Task struct {
 	Condition chan struct{}
 }
 
-var taskQueue = make(chan *Task)
-
 func (node *Node) Accept(
 	address string,
-	block func(key []byte, value []byte),
+	etcdWrite func(key []byte, value []byte),
 ) error {
-	println("dev debug")
-	go func() {
-		for {
-			task, ok := <-taskQueue
-			if !ok {
-				println("There was some error!")
-				break
-			}
-			block(task.Key, task.Value)
-			close(task.Condition)
-		}
-	}()
 
 	for {
 		// loop here cause port might be stuck open
@@ -148,7 +112,6 @@ func (node *Node) Accept(
 					if err != nil {
 						panic(err)
 					}
-
 					op := buffer[0]
 					if op == OpWrite {
 						err := client.Read(buffer[:12])
@@ -169,9 +132,15 @@ func (node *Node) Accept(
 							panic(err)
 						}
 
+						key := make([]byte, keySize)
+						value := make([]byte, valueSize)
+						copy(key, buffer[:keySize])
+						copy(value, buffer[keySize:(keySize+valueSize)])
+
 						go func() {
-							block(buffer[:keySize], buffer[keySize:(keySize+valueSize)])
-							response := make([]byte, 4)
+							etcdWrite(key, value)
+							response := make([]byte, 5)
+							buffer[0] = OpAck
 							binary.LittleEndian.PutUint32(response, commitIndex)
 							mutex.Lock()
 							err = client.Write(response)
@@ -180,6 +149,66 @@ func (node *Node) Accept(
 								panic(err)
 							}
 						}()
+					} else if op == OpForward {
+						err := client.Read(buffer[:8])
+						if err != nil {
+							panic(err)
+						}
+						keySize := binary.LittleEndian.Uint32(buffer[:4])
+						valueSize := binary.LittleEndian.Uint32(buffer[4:8])
+						required := int(keySize + valueSize)
+						if len(buffer) < required {
+							buffer = append(buffer, make([]byte, required-len(buffer))...)
+						}
+						err = client.Read(buffer[:(keySize + valueSize)])
+						key := make([]byte, keySize)
+						value := make([]byte, valueSize)
+						copy(key, buffer[:keySize])
+						copy(value, buffer[keySize:(keySize+valueSize)])
+						go func() {
+							node.Write(key, value, etcdWrite)
+						}()
+					} else if op == OpAck {
+						// 1
+						// 1 -> write to other nodes, collect acks, respond back to client
+						// 1 -> read full value -> respond back to client
+
+						// 1, 2, 3, 4, 5
+						// 2 -> 1 -> write to other nodes -> collect acks -> commit -> 2 gets commit -> responds back to client
+						// 2 -> 1 -> read full value -> send back to 2 -> repsond back to client
+
+						err = client.Read(buffer)
+						if err != nil {
+							panic(err)
+						}
+						commitIndex := binary.LittleEndian.Uint32(buffer)
+						go func() {
+							node.Log.Lock.Lock()
+							entry, exists := node.Log.Entries[commitIndex]
+							node.Log.Lock.Unlock()
+
+							if exists && atomic.AddUint32(&entry.acked, 1) == entry.majority {
+								close(entry.condition)
+								node.Log.Lock.Lock()
+								delete(node.Log.Entries, commitIndex)
+								node.Log.Lock.Unlock()
+
+								for i := range node.Clients {
+									go func(index int, client Client) {
+
+									}(i, node.Clients[i])
+								}
+							}
+						}()
+					} else if op == OpCommit {
+						err = client.Read(buffer)
+						if err != nil {
+							panic(err)
+						}
+						//commitIndex := binary.LittleEndian.Uint32(buffer)
+						go func() {
+
+						}()
 					}
 				}
 			}()
@@ -187,14 +216,35 @@ func (node *Node) Accept(
 	}
 }
 
-var CommitIndex uint32
+func (node *Node) Forward(
+	key []byte,
+	value []byte,
+	etcdWrite func(key []byte, value []byte),
+) {
+	// forward request to leader
+	if node.Index != node.Leader {
+		buffer := make([]byte, 9+len(key)+len(value))
+		buffer[0] = OpForward
+		binary.LittleEndian.PutUint32(buffer[1:5], uint32(len(key)))
+		binary.LittleEndian.PutUint32(buffer[5:9], uint32(len(value)))
+		var keyIndex = 9 + len(key)
+		copy(buffer[9:keyIndex], key)
+		copy(buffer[keyIndex:keyIndex+len(value)], value)
+		//var leader = node.Clients[node.Leader]
+		//leader.mutex.Lock()
+		//err := leader.Write(buffer)
+		//leader.mutex.Unlock()
+		// close condition here
+	} else {
+		node.Write(key, value, etcdWrite)
+	}
+}
 
 func (node *Node) Write(
 	key []byte,
 	value []byte,
-	block func(key []byte, value []byte),
+	etcdWrite func(key []byte, value []byte),
 ) {
-	//fmt.Printf("Total size: %d", len(value))
 	var segmentSize = int(math.Ceil(float64(len(value)) / float64(node.Segments)))
 	var segments = reedsolomon.AllocAligned(node.Segments+node.Parity, segmentSize)
 	var startIndex = 0
@@ -228,6 +278,7 @@ func (node *Node) Write(
 	node.Log.Lock.Lock()
 	node.Log.Entries[commitIndex] = entry
 	node.Log.Lock.Unlock()
+	etcdWrite(key, value)
 
 	for i := range node.Clients {
 		go func(index int, client Client) {
