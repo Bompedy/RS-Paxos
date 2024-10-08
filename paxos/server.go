4ee28d10be971d894136f9f86a02bbb92465bc03
money <lucasphone75@gmail.com>
2024-10-08 13:33:18 -0600

    try adding uuids to server

--- paxos/server.go
+++ paxos/server.go
@@ -10,7 +10,6 @@ import (
 	"runtime"
 	"sync"
 	"sync/atomic"
-	"time"
 )
 
 var OpPropose = uint8(0)
@@ -433,7 +432,7 @@ func (node *Node) ForwardWrite(
 		node.RequestWaiter[requestId] = channel
 		node.RequestLock.Unlock()
 		//println("Forwarded packet")
-		time.Sleep(2 * time.Second)
+		//time.Sleep(2 * time.Second)
 		<-channel
 		fmt.Printf("Forwarded packet id=%s\n", requestId.String())
 	} else {
