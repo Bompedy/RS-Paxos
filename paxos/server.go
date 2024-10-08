6cc53008904f99e5e6604b6d8a33fe974837ac59
money <lucasphone75@gmail.com>
2024-10-08 16:42:25 -0600

    try adding uuids to server

--- paxos/server.go
+++ paxos/server.go
@@ -243,7 +243,7 @@ func (node *Node) Accept(
 							}
 
 							node.Log.Lock.Unlock()
-							time.Sleep(5 * time.Second)
+							time.Sleep(10 * time.Millisecond)
 							//fmt.Printf("releasing lock?: %d\n", current)
 						}
 						//
