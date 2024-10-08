f88d236322b30fe688adac33f6b9de374cbf0d20
money <lucasphone75@gmail.com>
2024-10-08 12:13:08 -0600

    try adding uuids to server

--- paxos/server.go
+++ paxos/server.go
@@ -61,6 +61,7 @@ type CommitPacket struct {
 }
 
 func GetProposePacket(buffer []byte) ProposePacket {
+	println("Getting proposal packet")
 	//requestId := binary.LittleEndian.Uint32(buffer[:4])
 	var requestId uuid.UUID
 	copy(requestId[:], buffer[:16])
@@ -73,6 +74,7 @@ func GetProposePacket(buffer []byte) ProposePacket {
 	value := make([]byte, valueSize)
 	copy(key, buffer[keyStart:keyEnd])
 	copy(value, buffer[keyEnd:keyEnd+valueSize])
+	println("got it")
 	return ProposePacket{
 		Slot:      slot,
 		Key:       key,
