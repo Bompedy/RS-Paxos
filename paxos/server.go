b76a8b987dfa6370efa8ac20bf9a1687761a1260
money <lucasphone75@gmail.com>
2024-10-08 17:54:21 -0600

    swap to sync map

--- paxos/server.go
+++ paxos/server.go
@@ -9,6 +9,7 @@ import (
 	"net"
 	"sync"
 	"sync/atomic"
+	"time"
 )
 
 var OpPropose = uint8(0)
@@ -419,7 +420,7 @@ func (node *Node) Accept(
 									fmt.Printf("we are so stuck on %d\n", current)
 								}
 
-								//time.Sleep(5000 * time.Millisecond)
+								time.Sleep(5000 * time.Millisecond)
 							}
 							//
 							CommitIndex = current
