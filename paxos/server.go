7890f76e3bd6005efa825af7b65d79d4fbc9dfd9
money <lucasphone75@gmail.com>
2024-10-08 20:24:01 -0600

    swap to sync map

--- paxos/server.go
+++ paxos/server.go
@@ -300,6 +300,7 @@ func (node *Node) Accept(
 										//next := start + 1
 										nextValue, nextEntryExists := node.Entries.Load(next)
 										if !nextEntryExists {
+											next -= 1
 											//fmt.Printf("No entry breaking out %d\n", next)
 											break
 										}
