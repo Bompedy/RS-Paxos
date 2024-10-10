87153a6ae7ea2fad8cb6b9e5227ae4189f18c2fa
money <lucasphone75@gmail.com>
2024-10-09 23:11:26 -0600

    turn key back into byte array

--- paxos/server.go
+++ paxos/server.go
@@ -553,7 +553,7 @@ func (node *Node) Accept(
 						length := binary.LittleEndian.Uint32(buffer[1:5])
 						key := buffer[5 : 5+length]
 						keyString := string(key)
-						segment := buffer[5+length:]
+						segment := buffer[5+length : packetSize]
 						segmentMap[index].Store(keyString, segment)
 						count := 0
 						for i := uint32(1); i < node.Total; i++ {
